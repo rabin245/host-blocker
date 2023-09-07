6acc70046f6c67ef108cdf26805e3c1a5fa72132
aayussraut <aayussraut.ar@gmail.com>
2023-09-07 21:18:42 +0545

    try using zustand store outside the component

--- src/background-script/background.js
+++ src/background-script/background.js
@@ -1,6 +1,48 @@
 import fs from "fs";
+// import { create } from "zustand";
+// import { subscribeWithSelector } from "zustand/middleware";
+// import useFilterStore from "../state/filterState";
 
-console.log("hello this is from background script");
+// const globalFilterStatus = useFilterStore((state) => state.globalFilterStatus);
+// console.log("hello this is from background script");
+// console.log("this is test", globalFilterStatus);
+
+// const useStore = create(
+//   subscribeWithSelector(() => ({
+//     globalFilterStatus: false,
+//   }))
+// );
+
+// const globalFilterStatus = useFilterStore.getState().globalFilterStatus;
+// console.log("Global Filter Status", globalFilterStatus);
+
+// const unsubscribe = useFilterStore.subscribe(
+//   (state) => state, // Subscribe to all state changes
+//   (newState, prevState) => {
+//     console.log("State changed:", newState);
+//     console.log("Previous state:", prevState);
+//     // Handle state changes here, if needed
+//   }
+// {
+//   fireImmediately: true,
+// }
+// );
+
+// const pawStatus = useFilterStore.getState().globalFilterStatus;
+// console.log("Paw Status", pawStatus);
+
+// const unsub = useFilterStore.subscribe(
+//   (state) => state.globalFilterStatus,
+//   console.log
+// );
+
+// const unsub1 = useFilterStore.subscribe(
+//   (state) => state.globalFilterStatus,
+//   (newVal, old) => console.log(newVal, old)
+// );
+
+// unsub();
+// unsub1();
 
 chrome.declarativeNetRequest.onRuleMatchedDebug.addListener((e) => {
   const msg = `Navigation blocked to ${e.request.url} on tab ${e.request.tabId}.`;
